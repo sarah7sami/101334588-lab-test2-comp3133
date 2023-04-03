df0a6eb3c7fb12702946138403ffda69b616dfab
Sarah Sami <sarah7sami@gmail.com>
2023-04-02 22:16:42 -0400

    Fix build issues

--- src/app/mission-filter/mission-filter.component.ts
+++ src/app/mission-filter/mission-filter.component.ts
@@ -6,9 +6,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./mission-filter.component.css']
 })
 export class MissionFilterComponent {
+  years: number[] = [];
+  selectedYear: number | undefined;
+
   @Output() yearSelected = new EventEmitter<number>();
 
-  onYearSelected(year: number) {
-    this.yearSelected.emit(year);
+  constructor() {
+    const currentYear = new Date().getFullYear();
+    for (let year = currentYear; year >= 2006; year--) {
+      this.years.push(year);
+    }
+  }
+
+  onYearChange(year: number) {
+    this.selectedYear = year;
+    this.yearSelected.emit(this.selectedYear);
   }
 }
--- src/app/mission-list/mission-list.component.ts
+++ src/app/mission-list/mission-list.component.ts
@@ -24,3 +24,4 @@ export class MissionListComponent implements OnInit {
     });
   }
 }
+
