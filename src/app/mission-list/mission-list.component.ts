fed65aa415f16e77da7f3daf92dd6532193fa9be
Sarah Sami <sarah7sami@gmail.com>
2023-04-02 21:56:24 -0400

    Implement MissionListComponent

--- src/app/mission-list/mission-list.component.ts
+++ src/app/mission-list/mission-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mission-list',
   templateUrl: './mission-list.component.html',
   styleUrls: ['./mission-list.component.css']
 })
-export class MissionListComponent {
+export class MissionListComponent implements OnInit {
+  missions: any[] | undefined;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('https://api.spacexdata.com/v3/launches').subscribe(data => {
+      this.missions = data.map(mission => {
+        return {
+          missionName: mission.mission_name,
+          launchYear: mission.launch_year,
+          details: mission.details,
+          missionPatchSmall: mission.links.mission_patch_small
+        };
+      });
+    });
+  }
 }
