93e9527365fb85a54942e4a0d8ec61d3190d31be
Sarah Sami <sarah7sami@gmail.com>
2023-04-03 01:22:59 -0400

    Add HttpClientModule to imports (im stupid and im crying)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { MissionListComponent } from './mission-list/mission-list.component';
 import { MissionFilterComponent } from './mission-filter/mission-filter.component';
 import { YearFilterPipe } from './year-filter.pipe';
@@ -18,6 +19,7 @@ import { YearFilterPipe } from './year-filter.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule, 
     AppRoutingModule
   ],
   providers: [],
